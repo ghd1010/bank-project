998e1cb8cfb5af917526e1202f98e9ce7fdda5e5
ghd1010 <140907325+ghd1010@users.noreply.github.com>
2025-03-10 02:27:20 +0300

    updated add_customer_to_csv() and add error handling to it

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ my_csv_file = "bank.csv"
 fields = ['account_id', 'frst_name', 'last_name', 'password', 'balance_checking', 'balance_savings']  
 
 # data rows of csv file  
-rows = [ [10001, 'suresh', 'sigera', 'juagw362', 1000, 10000],  
+rows = [[10001, 'suresh', 'sigera', 'juagw362', 1000, 10000],  
         [10002, 'james', 'taylor', 'idh36%@#FGd', 10000, 10000],  
         [10003, 'melvin', 'gordon', 'uYWE732g4ga1', 2000, 20000],  
         [10004, 'stacey', 'abrams', 'DEU8_qw3y72$', 2000, 20000],  
@@ -32,6 +32,7 @@ class Customer:
         self.last_name = last_name
         self.password = password
 
+    # create a default ID to customers when we add them
     def create_customer_id(self):
         number_of_existing_customers = 0 # initialize variable
         with open(my_csv_file, mode ='r') as csvfile:
@@ -44,24 +45,40 @@ class Customer:
         new_id = 10001 + number_of_existing_customers # generate new ID according to existing customers
         return new_id
 
+    # append the new customer to the csv file
     def add_customer_to_csv(self, balance_checking=0, balance_savings=0):
-        # create list
-        new_customer = [self.create_customer_id(), 
-                        self.first_name,
-                        self.last_name,
-                        self.password,
-                        balance_checking,
-                        balance_savings]
+        try:
+            # check if the customer is already exist
+            with open(my_csv_file, mode='r') as csvfile:
+                content = csv.reader(csvfile)
+                next(content)  # Skip header
+                for line in content:
+                    if line[1] == self.first_name and line[2] == self.last_name:
+                        print(f"Sorry, customer with name ({self.first_name} {self.last_name}) is already exists.")
+                        return 
+                    
+            # create list to add customer
+            new_customer = [self.create_customer_id(), 
+                            self.first_name,
+                            self.last_name,
+                            self.password,
+                            balance_checking,
+                            balance_savings]
+            
+            # add the customer
+            with open(my_csv_file, mode='a') as csvfile:  # 'a' stands for append
+                # creating a csv writer object  
+                csvwriter = csv.writer(csvfile)  
+                # writing the data row
+                csvwriter.writerow(new_customer)
+                
+        except FileNotFoundError:
+            print(f"Sorry, the csv file ({my_csv_file}) is not found. Please make sure you have the correct file.")
         
-        with open(my_csv_file, 'a') as csvfile:  # 'a' stands for append
-            # creating a csv writer object  
-            csvwriter = csv.writer(csvfile)  
-            # writing the data row
-            csvwriter.writerow(new_customer)
 
 # just testing my function here
-customer = Customer('Ghada', 'Almutairi', 'GH124')
-customer.add_customer_to_csv()
+# customer = Customer('Ghada', 'Almutairi', 'GH124')
+# customer.add_customer_to_csv()
 
 # class Account:
 #     def __init__(self, account_id, balance_checking, balance_savings):
